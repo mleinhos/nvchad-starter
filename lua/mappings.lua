7b48615d0def336874bd3ab3554689e143fb2c19
Matt Leinhos <mleinhos@paloaltonetworks.com>
2024-03-12 16:57:31 -0400

    Cscope fixes

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -13,4 +13,4 @@ end, { desc = "File Format with conform" })
 map("i", "jk", "<ESC>", { desc = "Escape insert mode" })
 
 -- vim.cmd("map <F5> :!cscope -Rb<CR>:cs reset<CR><CR>")
-map("n", "<F5>", ":CScope -Rb<CR>:Cscope reset<CR><CR>")
+map("n", "<F5>", ":Cscope build<CR>:Cscope reset<CR><CR>")
